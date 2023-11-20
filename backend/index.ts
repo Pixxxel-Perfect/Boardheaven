c13d83ef769e58ee63140af6ae2927edf9770f3d
Philip-Bachl <bac22566@spengergasse.at>
2023-11-20 11:12:06 +0100

    Add Rooms and first Websocket impl.
    Signed-off by: Philip-Bachl, Matteo

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,49 @@
+import { ServerWebSocket } from "bun";
+
+class WsData {
+    public roomId: string = "";
+
+    constructor(roomId: string) {
+        this.roomId = roomId;
+    }
+}
+
+class Player {
+    public ws: ServerWebSocket<unknown>;
+
+    constructor(ws: ServerWebSocket<WsData>) {
+        this.ws = ws;
+    }
+
+    get roomId() {
+        return (this.ws.data as WsData).roomId;
+    }
+}
+
+class Room {
+    public websockets: ServerWebSocket<unknown>[] = [];
+    public gameMaster: Player;
+    constructor(gameMaster: Player, websockets?: ServerWebSocket<unknown>[]) {
+        this.gameMaster = gameMaster;
+        if (websockets) {
+            this.websockets = websockets;
+        }
+    }
+}
+
+const rooms: Room = [];
+
 Bun.serve({
     fetch(req: Request): Response | Promise<Response> {
-        if (this.upgrade(req)) {
+        if (this.upgrade(req, {data: new WsData(req.url)})) {
             console.log("websocket online")
         }
       return new Response("Connect via Websocket please.");
     },
     websocket: {
+        open(ws) {
+            //TODO
+        },
         message(ws, message) {
             console.log(message);
             ws.send(message);
