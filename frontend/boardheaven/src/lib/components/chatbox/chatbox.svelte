7edb31b1aebe89051f45c5cfd683e5fd54281724
mutantboy <philipp.khachik@gmail.com>
2024-01-15 00:20:00 +0100

    Chatbox, todo: connect with websocket

---
+++ frontend/boardheaven/src/lib/components/chatbox/chatbox.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { chatStore } from "./chatStore";
+  import Message from "./message.svelte";
+  import type { ParentSendMessage } from "../../utils";
+
+  export let wsSendMessage: ParentSendMessage;
+
+  let inputFiled: HTMLInputElement;
+  let messages: string[] = [];
+  let myMessage: string = "";
+
+  onMount(() => {
+    const unsubscribe = chatStore.subscribe((value) => {
+      messages = value;
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  });
+
+  function sendMessage() {
+    // for now we can not use this function because of the problem described above!
+    wsSendMessage(myMessage);
+
+    console.log(myMessage);
+
+    myMessage = "";
+    if (inputFiled) {
+      inputFiled.focus();
+    }
+  }
+</script>
+
+<div class="msg">
+  {#each messages as msg, index}
+    <Message name="Max Mustermann" message={msg} />
+  {/each}
+  <input type="text" bind:value={myMessage} bind:this={inputFiled} />
+  <button on:click={sendMessage}>send</button>
+</div>
---
+++ frontend/boardheaven/src/lib/components/chatbox/message.svelte
@@ -0,0 +1,15 @@
+<script>
+  /**
+   * @type {string}
+   */
+  export let name;
+  /**
+   * @type {string}
+   */
+  export let message;
+</script>
+
+<div>
+  <div><h3>{name}</h3></div>
+  <div><p>{message}</p></div>
+</div>
