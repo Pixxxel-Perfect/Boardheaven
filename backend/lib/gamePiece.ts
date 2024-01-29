0a906e24d1344e2915b882cbcfb5bb645acc37bf
Philip-Bachl <bac22566@spengergasse.at>
2024-01-29 10:08:16 +0100

    some pos logic and a lot of TODOs
    Signed-off by: Philip-Bachl

--- backend/lib/gamePiece.ts
+++ backend/lib/gamePiece.ts
@@ -3,23 +3,30 @@ import { Player } from "./player";
 class GamePiece {
     
     public static of(piece: GamePiece): GamePiece {
-        return new GamePiece(piece.owner, piece.homePos, piece.firstPos);
+        return new GamePiece(piece.owner, piece.homePos);
     }
     
     public pos: number;
-    
-    constructor(public owner: Player, public homePos: number, public firstPos: number) {
-        this.pos = homePos;
+
+    public get color() {
+        return this.owner.color;
     }
 
-    get color() {
-        return this.owner.color;
+    public get initPos() {
+        return this.color * 10 + 1;
+    }
+    
+    constructor(public owner: Player, public homePos: number) {
+        this.pos = homePos;
     }
 
     public capture() {
         this.pos = this.homePos;
     }
     
+    public init() {
+        this.pos = this.initPos;
+    }
 }
 
 export { GamePiece };
